10e50fe938432191f5b83ac6b04449ae748dee85
SOunit <workbookwork7@gmail.com>
2022-02-20 13:12:59 -0800

    fix type error

--- components/organisms/menu-button.tsx
+++ components/organisms/menu-button.tsx
@@ -1,17 +1,20 @@
 import { Platform } from "react-native";
+import { NavigationDrawerProp } from "react-navigation-drawer";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../common/CustomHeaderButton";
 
-const MenuButton = (navigation) => (
-  <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
-    <Item
-      title="Menu"
-      iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
-      onPress={() => {
-        navigation.toggleDrawer();
-      }}
-    />
-  </HeaderButtons>
-);
+const MenuButton = (navigation: NavigationDrawerProp) => {
+  return (
+    <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+      <Item
+        title="Menu"
+        iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+        onPress={() => {
+          navigation.toggleDrawer();
+        }}
+      />
+    </HeaderButtons>
+  );
+};
 
 export default MenuButton;
